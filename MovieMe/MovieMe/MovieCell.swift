4c5fd6d5ce97be1dd853f5a24c7b2556b0792dd8
Danyal <d.s.rizvi@gmail.com>
2016-01-26 18:36:56 -0600

    hw done

---
+++ MovieMe/MovieMe/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  MovieMe
+//
+//  Created by Focus on 1/26/16.
+//  Copyright © 2016 Danyal Rizvi. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
